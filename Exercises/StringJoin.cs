528d41681132d501aa93f3d0506e09ed888d3556
João Pedro Brasil <116403431+JoaoPBrasil@users.noreply.github.com>
2024-12-12 12:08:02 -0300

    Create StringJoin.cs

---
+++ Exercises/StringJoin.cs
@@ -0,0 +1,11 @@
+class Progam
+{
+    static void Main()
+    {
+        string name = " ";
+        string[] names = {"Joao", "Pedro", "Brasil"};
+
+        Console.WriteLine(string.Join('+', names));
+
+    }
+}
