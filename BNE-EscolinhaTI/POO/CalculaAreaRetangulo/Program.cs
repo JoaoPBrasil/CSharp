846907838612fa4ce7f5f4e135a0aef5d46edadb
João Pedro Brasil <116403431+JoaoPBrasil@users.noreply.github.com>
2024-12-19 14:14:42 -0300

    Add files via upload

---
+++ BNE-EscolinhaTI/POO/CalculaAreaRetangulo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaRetangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo = new Retangulo();
+            Console.WriteLine("Calula a area de um triangulo");
+
+            Console.Write("Informe a altura: ");
+            retangulo.Altura = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Informe a base: ");
+            retangulo.Base = Convert.ToDouble(Console.ReadLine());
+
+            retangulo.ExibeDados();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ BNE-EscolinhaTI/POO/CalculaAreaRetangulo/Retangulo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaRetangulo
+{
+    public class Retangulo
+    {
+        public Retangulo()
+        {
+            this.Altura = 0;
+            this.Base = 0; 
+        }
+        //base do retângulo
+        private Double baseret;
+
+        public Double Base
+        {
+            get { return baseret; }
+            set { 
+                if (value >= 0) baseret = value;
+                else baseret = 0;
+            }
+        }
+        //altura do retângulo
+
+        private Double altret;
+
+        public Double Altura
+        {
+            get { return altret; }
+            set { 
+                if (value >= 0) altret = value; 
+                else altret = 0;
+            }
+        }
+
+        public Double Area
+        {
+            get { return altret * baseret; }
+        }
+
+        public void ExibeDados()
+        {
+            Console.WriteLine($"Base: {this.Base}");
+            Console.WriteLine($"Altura: {this.Altura}");
+            Console.WriteLine($"Área: {this.Area}");
+        }
+
+
+
+    }
+}
