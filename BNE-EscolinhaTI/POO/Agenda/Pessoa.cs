2a0586c15e48ab654a7de32aa15f75a741eaea3d
João Pedro Brasil <116403431+JoaoPBrasil@users.noreply.github.com>
2024-12-19 14:36:15 -0300

    Add files via upload

---
+++ BNE-EscolinhaTI/POO/Agenda/Pessoa.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda
+{
+    public class Pessoa
+    {
+        private String _nome;
+        private String _email;
+        private String _telefone;
+
+        public Pessoa(string nome, string email, string telefone)
+        {
+            this.nome = nome;
+            this.email = email;
+            this.telefone = telefone;
+        }
+
+        public Pessoa()
+        {
+            this.nome = "";
+            this.email = "";
+            this.telefone = "";
+
+        }
+
+        public string nome
+        {
+            get
+            {
+                return this._nome;
+            }
+            set
+            {
+                this._nome = value;
+            }
+        }
+
+        public String email
+        {
+            get
+            {
+                return this._email;
+            }
+            set
+            {
+                this.email = value;
+            }
+        }
+
+        public String telefone
+        {
+            get
+            {
+                return this._telefone;
+            }
+            set
+            {
+                this._telefone = value;
+            }
+        }
+
+        public void exibeDados()
+        {
+            Console.Clear();
+            Console.WriteLine($"Nome: {this.nome}");
+            Console.WriteLine($"Email: {this.email}");
+            Console.WriteLine($"Telefone: {this.telefone}");
+        }
+    }
+}
---
+++ BNE-EscolinhaTI/POO/Agenda/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa("Joao", "Email@gmail.com", "22 22222-2222");
+            Pessoa p2 = new Pessoa();
+            p1.exibeDados();
+            Console.ReadKey();            
+        }
+    }
+}
